36f8fba7e1096a70c3739e053b565a759ce78b06
ryoha000 <48485650+ryoha000@users.noreply.github.com>
2020-05-30 02:49:04 +0900

    create saleinfoitem(card

---
+++ ESClient/ViewModels/Main/Home/ReleaseDateViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ESClient.ViewModels.Main.Home
+{
+    class ReleaseDateViewModel : Prism.Mvvm.BindableBase
+    {
+    }
+}
---
+++ ESClient/ViewModels/Main/Home/SaleInfoViewModel.cs
@@ -0,0 +1,14 @@
+﻿using Prism.Mvvm;
+
+namespace ESClient.ViewModels.Main.Home
+{
+    class SaleInfoViewModel : BindableBase
+    {
+        private string _campaign = "キャンペーン名";
+        public string Campaign
+        {
+            get { return _campaign; }
+            set { SetProperty(ref _campaign, value); }
+        }
+    }
+}
