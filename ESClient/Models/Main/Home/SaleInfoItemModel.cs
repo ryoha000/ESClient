0243042bff27b306bc68eb41f25e074f0ab92bfc
ryoha000 <48485650+ryoha000@users.noreply.github.com>
2020-05-30 06:10:33 +0900

    a

--- ESClient/Models/Main/Home/SaleInfoItemModel.cs
+++ ESClient/Models/Main/Home/SaleInfoItemModel.cs
@@ -11,80 +11,96 @@ namespace ESClient.Models.Main.Home
     {
         public static BitmapFrame recognizeFace()
         {
-            using (Mat mat = new Mat("../../../Resources/ESClient_demo_image.jpg"))
+            //using (Mat mat = new Mat("../../../Resources/ESClient_demo_image.jpg"))
+            using (Mat mat = new Mat(@"E:\Users\ユウヤ\Pictures\コメント 2020-05-30 054140.png"))
             {
-                Mat newMat = new Mat();
-                var ratio = mat.Width / mat.Height;
-                int bairitu;
+                double ratio = (double)mat.Width / (double)mat.Height;
+                double bairitu;
                 // 横長の時
                 if (ratio > 2)
                 {
-                    bairitu = 125 / mat.Height;
+                    bairitu = (double)125 / (double)mat.Height;
                 }
                 else
                 {
-                    bairitu = 250 / mat.Width;
+                    bairitu = (double)250 / (double)mat.Width;
                 }
-                System.Diagnostics.Debug.WriteLine(bairitu);
-                //Cv2.Resize(mat, newMat, new OpenCvSharp.Size(mat.Width * bairitu, mat.Height * bairitu), 0, 0, InterpolationFlags.Cubic);
-                Cv2.Resize(mat, newMat, OpenCvSharp.Size.Zero, bairitu, bairitu, InterpolationFlags.Cubic);
+                Mat newMat = new Mat();
+                if (bairitu > 1)
+                {
+                    Cv2.Resize(mat, newMat, OpenCvSharp.Size.Zero, bairitu, bairitu, InterpolationFlags.Cubic);
+                }
+                else
+                {
+                    Cv2.Resize(mat, newMat, OpenCvSharp.Size.Zero, bairitu, bairitu, InterpolationFlags.Area);
+                }
+                Cv2.ImShow("mat", mat);
+                Cv2.ImShow("newMat", newMat);
                 using (CascadeClassifier cascade = new CascadeClassifier("../../../Resources/lbpcascade_animeface.xml"))
                 {
-                    //foreach (OpenCvSharp.Rect rectFace in cascade.DetectMultiScale(mat))
+                    var maxSquare = 0;
+                    var maxX = 0;
+                    var maxY = 0;
+                    var maxWidth = 0;
+                    var maxHeight = 0;
+                    foreach (OpenCvSharp.Rect rectFace in cascade.DetectMultiScale(mat))
+                    {
+                        if (maxSquare < rectFace.Height * rectFace.Width)
+                        {
+                            maxSquare = rectFace.Height * rectFace.Width;
+                            maxX = rectFace.X;
+                            maxY = rectFace.Y;
+                            maxHeight = rectFace.Height;
+                            maxWidth = rectFace.Width;
+                        }
+                    }
                     if (cascade.DetectMultiScale(newMat).Length < 1)
                     {
                         return null;
                     }
-                    OpenCvSharp.Rect rectFace = cascade.DetectMultiScale(newMat)[0];
+                    var centerX = maxX + maxWidth / 2;
+                    var centerY = maxY + maxHeight / 2;
+                    Mat convert_mat = new Mat();
+                    // 横長
+                    if (ratio >= 2)
                     {
-
-                        var centerX = rectFace.X + rectFace.Width / 2;
-                        var centerY = rectFace.Y + rectFace.Height / 2;
-                        Mat convert_mat = new Mat();
-                        // 横長
-                        if (ratio >= 2)
+                        // 横長すぎ
+                        if (centerX > 125 && newMat.Width - centerX > 125)
                         {
-                            // 横長すぎ
-                            if (centerX > 125 && newMat.Width - centerX > 125)
-                            {
-                                convert_mat = mat.Clone(new Rect(centerX - 125, 0, centerX + 125, 125));
-                            }
-                            else if (centerX < 125)
-                            {
-                                convert_mat = mat.Clone(new Rect(0, 0, 250, 125));
-                            }
-                            else
-                            {
-                                convert_mat = mat.Clone(new Rect(newMat.Width - 250, 0, newMat.Width, 125));
-                            }
+                            convert_mat = newMat.Clone(new Rect(centerX - 125, 0, centerX + 125, 125));
                         }
-                        // 縦長
+                        else if (centerX < 125)
+                        {
+                            convert_mat = newMat.Clone(new Rect(0, 0, 250, 125));
+                        }
                         else
                         {
-                            if (centerY > 63 && newMat.Width - centerY > 63)
-                            {
-                                convert_mat = mat.Clone(new Rect(0, centerY - 63, 250, centerY + 63));
-                            }
-                            else if (centerY < 63)
-                            {
-                                convert_mat = mat.Clone(new Rect(0, 0, 250, 125));
-                            }
-                            else
-                            {
-                                convert_mat = mat.Clone(new Rect(0, newMat.Height - 125, 250, newMat.Height));
-                            }
+                            convert_mat = newMat.Clone(new Rect(newMat.Width - 250, 0, newMat.Width, 125));
                         }
-                        // 見つかった場所に赤枠を表示
-                        var rect = new OpenCvSharp.Rect(rectFace.X, rectFace.Y, rectFace.Width, rectFace.Height);
-                        Cv2.Rectangle(mat, rect, new OpenCvSharp.Scalar(0, 0, 255), 2);
-                Cv2.ImShow("sample_show", newMat);
-                Bitmap bmp = BitmapConverter.ToBitmap(convert_mat);
-                using (Stream st = new MemoryStream())
-                {
-                    bmp.Save(st, ImageFormat.Bmp);
-                    st.Seek(0, SeekOrigin.Begin);
-                    return BitmapFrame.Create(st, BitmapCreateOptions.None, BitmapCacheOption.OnLoad);
-                }
+                    }
+                    // 縦長
+                    else
+                    {
+                        if (centerY > 63 && newMat.Width - centerY > 63)
+                        {
+                            convert_mat = newMat.Clone(new Rect(0, centerY - 63, 250, centerY + 63));
+                        }
+                        else if (centerY < 63)
+                        {
+                            convert_mat = newMat.Clone(new Rect(0, 0, 250, 125));
+                        }
+                        else
+                        {
+                            convert_mat = newMat.Clone(new Rect(0, newMat.Height - 125, 250, newMat.Height));
+                        }
+                    }
+                    Cv2.ImShow("convert_mat", convert_mat);
+                    Bitmap bmp = BitmapConverter.ToBitmap(convert_mat);
+                    using (Stream st = new MemoryStream())
+                    {
+                        bmp.Save(st, ImageFormat.Bmp);
+                        st.Seek(0, SeekOrigin.Begin);
+                        return BitmapFrame.Create(st, BitmapCreateOptions.None, BitmapCacheOption.OnLoad);
                     }
                 }
             }
